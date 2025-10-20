91db180b4c358a14db30a53b44517a9e9e17be6d
hearsch gaikwad <hearschdevops@gmail.com>
2025-10-20 18:33:31 +0530

    Update file app.py

--- Conference-setup/app.py
+++ Conference-setup/app.py
@@ -11,7 +11,7 @@ CORS(app)
 
 # PostgreSQL Database Configuration
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 
-    'postgresql://conf_user:conf_pass@postgres-svc:5432/conference_db')
+    'postgresql://conf_user:conf_pass@postgres:5432/conference_db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
